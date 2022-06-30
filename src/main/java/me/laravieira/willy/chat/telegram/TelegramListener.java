7afaffa161d2c73b635d5f2f5e9a424889d72b9d
Lara Vieira <contato@laravieira.me>
2022-06-30 02:34:16 -0300

    Fix Telegram bad request error.

--- src/main/java/me/laravieira/willy/chat/telegram/TelegramListener.java
+++ src/main/java/me/laravieira/willy/chat/telegram/TelegramListener.java
@@ -1,6 +1,7 @@
 package me.laravieira.willy.chat.telegram;
 
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import me.laravieira.willy.Willy;
@@ -24,15 +25,19 @@ public class TelegramListener implements UpdatesListener {
 
     public void onTextMessageReceived(@NotNull Update update) {
         Message msg = update.message();
-        UUID id = UUID.nameUUIDFromBytes(("telegram-"+msg.chat().id()).getBytes());
+        Chat chat = msg.chat();
+        UUID id = UUID.nameUUIDFromBytes(("telegram-"+chat.id()).getBytes());
 
-        TelegramSender sender = new TelegramSender(msg.chat());
-        ContextStorage.of(id).setSender(sender);
+        Thread messageHandler = new Thread(() -> {
+            TelegramSender sender = new TelegramSender(chat);
+            ContextStorage.of(id).setSender(sender);
 
-        TelegramMessage message = new TelegramMessage(id, msg, PassedInterval.DISABLE);
-        MessageStorage.add(message);
+            TelegramMessage message = new TelegramMessage(id, msg, PassedInterval.DISABLE);
+            MessageStorage.add(message);
 
-        ContextStorage.of(message.getContext()).getWatson().getSender().sendText(message.getText());
-        Willy.getLogger().getConsole().config(id + ": " + msg);
+            ContextStorage.of(message.getContext()).getWatson().getSender().sendText(message.getText());
+        });
+        messageHandler.setDaemon(true);
+        messageHandler.start();
     }
 }

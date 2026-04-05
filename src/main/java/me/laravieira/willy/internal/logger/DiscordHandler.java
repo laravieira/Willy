424d7922cd90c481cb4ce4dfe55637b3a51f0b7a
Lara Vieira <contato@laravieira.me>
2026-04-05 17:40:47 -0300

    Fix Discord connection reset error and improve Discord loggers.

--- src/main/java/me/laravieira/willy/internal/logger/DiscordHandler.java
+++ src/main/java/me/laravieira/willy/internal/logger/DiscordHandler.java
@@ -1,12 +1,6 @@
 package me.laravieira.willy.internal.logger;
 
-import discord4j.common.util.Snowflake;
-import discord4j.core.GatewayDiscordClient;
-import discord4j.core.object.entity.channel.MessageChannel;
-import me.laravieira.willy.Willy;
-import me.laravieira.willy.chat.discord.Discord;
-import me.laravieira.willy.internal.Config;
-import reactor.core.scheduler.Schedulers;
+import discord4j.core.object.entity.channel.GuildMessageChannel;
 
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
@@ -14,21 +8,21 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 class DiscordHandler extends Handler {
+    private final GuildMessageChannel channel;
+
+    public DiscordHandler(GuildMessageChannel channel) {
+        this.channel = channel;
+    }
+
     @Override
     public void publish(LogRecord record) {
         if(record.getMessage().isEmpty() || record.getLevel().intValue() < Level.INFO.intValue())
             return;
 
-        GatewayDiscordClient gateway = Discord.getBotGateway();
-        if(gateway == null || !Config.has("discord.admin.log"))
-            return;
+        if(channel == null) return;
 
         String message = "```yaml\r\n["+record.getLevel()+"] "+record.getMessage()+"```";
-        gateway.getChannelById(Snowflake.of(Config.getLong("discord.admin.log")))
-            .publishOn(Schedulers.boundedElastic())
-            .doOnError(error -> Willy.getLogger().fine(error.getMessage()))
-            .doOnSuccess(channel -> ((MessageChannel)channel).createMessage(message).subscribe())
-            .block();
+        channel.createMessage(message).block();
     }
 
     @Override
